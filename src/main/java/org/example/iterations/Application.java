9c1f4fe66277e1d5a351eeeac16517394d854f4e
Titus Muthomi <muthomititus+github@gmail.com>
2024-01-26 10:03:50 +0300

    Add lambda expressions

--- src/main/java/org/example/iterations/Application.java
+++ src/main/java/org/example/iterations/Application.java
@@ -5,9 +5,7 @@ import org.example.introduction.Room;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
-import java.util.Iterator;
-import java.util.function.Consumer;
-import java.util.function.Predicate;
+
 
 public class Application {
     public static void main(String[] args) {
@@ -20,19 +18,13 @@ public class Application {
         oxford.setPetFriendly(true);
         cambridge.setPetFriendly(true);
 
-        rooms.stream()
-                .filter(new Predicate<Room>() {
-                    @Override
-                    public boolean test(Room room) {
-                        return room.isPetFriendly();
-                    }
-                }).forEach(new Consumer<Room>() {
-                    @Override
-                    public void accept(Room room) {
-                        System.out.println(room.getName());
-                    }
-                });
+        rooms.stream().filter(Room::isPetFriendly).forEach(room -> System.out.println(room.getName()));
 
+
+//        rooms.stream().filter(room -> room.isPetFriendly()).forEach(room -> System.out.println(room.getName()));
 
     }
+
 }
+
+
