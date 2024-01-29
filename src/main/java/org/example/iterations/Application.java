8342d8fbcafa3505c3d174c7d97e214d4507496e
Titus Muthomi <muthomititus+github@gmail.com>
2024-01-29 14:05:19 +0300

    Add queue interface

--- src/main/java/org/example/iterations/Application.java
+++ src/main/java/org/example/iterations/Application.java
@@ -1,6 +1,7 @@
 package org.example.iterations;
 
 import org.example.introduction.Room;
+import org.example.list.Guest;
 
 import java.util.*;
 import java.util.stream.Collectors;
@@ -8,28 +9,61 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        Room cambridge = new Room("Cambridge", "Premier Room", 4, 175.00);
-        Room manchester = new Room("manchester", "Suite", 5, 250);
-        Room piccadilly = new Room("picadily", "Guest Room", 3, 125.0);
-        Room oxford = new Room("oxford", "Guest Room", 6, 250);
-        Room oxfordDuplicate = new Room("oxford", "Guest Room", 6, 250);
+        Guest john = new Guest("John", "Doe", false);
+        Guest maria = new Guest("Maria", "Doe", true);
+        Guest sonia = new Guest("Sonia", "Doe", true);
+        Guest siri = new Guest("Siri", "Doe", true);
+        Guest bob = new Guest("Bob", "Doe", false);
 
-        Set<Room> rooms = new HashSet<>();
-        Set<Room> otherRooms = Set.of(piccadilly, oxford);
-//        otherRooms.add(cambridge);
+        Comparator<Guest> programComp =     Comparator.comparing(Guest::isLoyaltyProgramMember).reversed();
 
 
-        rooms.add(piccadilly);
-        rooms.add(oxford);
-        rooms.add(oxford);
-        rooms.add(oxfordDuplicate);
-        rooms.add(manchester);
+        Queue<Guest> checkinQueue = new PriorityQueue<>(programComp);
+//        Queue<Guest> checkinQueue = new ArrayDeque<>();
+//        checkinQueue.offer(john);
+//        checkinQueue.offer(maria);
+//        checkinQueue.offer(sonia);
+//        checkinQueue.offer(siri);
+//        print(checkinQueue);
+////        remove returns exception and poll returns null
+//        Guest guest = checkinQueue.poll();
+//        print(checkinQueue);
+//        System.out.println(guest);
+//
+//        Guest guest1 = checkinQueue.peek();
+//        print(checkinQueue);
+//        System.out.println(guest1);
+//        methods that throw an error
+        checkinQueue.add(john);
+        checkinQueue.add(maria);
+        checkinQueue.add(sonia);
+        checkinQueue.add(siri);
+        checkinQueue.add(maria);
+        print(checkinQueue);
+//        remove returns exception and poll returns null
+        Guest guest = checkinQueue.remove();
+        print(checkinQueue);
+        System.out.println(guest);
+
+        Guest guest1 = checkinQueue.element();
+        print(checkinQueue);
+        System.out.println(guest1);
+
+//        poll removes while peek just peeks
 
 
-        Set<Room> moreRooms = Set.copyOf(rooms);
+    }
 
-        moreRooms.stream().map(r -> r.getName()).forEach(System.out::println);
+    public static void print(Queue<Guest> queue) {
 
+        System.out.format("%n--Queue Contents--%n");
+
+        int x = 0;
+        for (Guest guest : queue) {
+            System.out.format("%x: %s %s %n", x++, guest.toString(), x == 1 ? "(Head)" : "");
+        }
+
+        System.out.println("");
 
     }
 
