6cf0df57a657e437f0fad596c538c7138160ca55
Titus Muthomi <muthomititus+github@gmail.com>
2024-01-29 14:19:26 +0300

    Implement stack with dequeu

--- src/main/java/org/example/iterations/Application.java
+++ src/main/java/org/example/iterations/Application.java
@@ -9,60 +9,30 @@ import java.util.stream.Collectors;
 
 public class Application {
     public static void main(String[] args) {
-        Guest john = new Guest("John", "Doe", false);
-        Guest maria = new Guest("Maria", "Doe", true);
-        Guest sonia = new Guest("Sonia", "Doe", true);
-        Guest siri = new Guest("Siri", "Doe", true);
-        Guest bob = new Guest("Bob", "Doe", false);
 
-        Comparator<Guest> programComp =     Comparator.comparing(Guest::isLoyaltyProgramMember).reversed();
+        Deque<String> messageStack = new ArrayDeque<>();
+        messageStack.push("Message 1");
+        messageStack.push("Message 2");
+        messageStack.push("Message 3");
+        messageStack.push("Message 4");
 
+        print(messageStack);
+        System.out.println(messageStack.pop());
+        System.out.println(messageStack.pop());
+        print(messageStack);
+        messageStack.push("Message 5");
+        System.out.println(messageStack.peek());
 
-        Queue<Guest> checkinQueue = new PriorityQueue<>(programComp);
-//        Queue<Guest> checkinQueue = new ArrayDeque<>();
-//        checkinQueue.offer(john);
-//        checkinQueue.offer(maria);
-//        checkinQueue.offer(sonia);
-//        checkinQueue.offer(siri);
-//        print(checkinQueue);
-////        remove returns exception and poll returns null
-//        Guest guest = checkinQueue.poll();
-//        print(checkinQueue);
-//        System.out.println(guest);
-//
-//        Guest guest1 = checkinQueue.peek();
-//        print(checkinQueue);
-//        System.out.println(guest1);
-//        methods that throw an error
-        checkinQueue.add(john);
-        checkinQueue.add(maria);
-        checkinQueue.add(sonia);
-        checkinQueue.add(siri);
-        checkinQueue.add(maria);
-        print(checkinQueue);
-//        remove returns exception and poll returns null
-        Guest guest = checkinQueue.remove();
-        print(checkinQueue);
-        System.out.println(guest);
-
-        Guest guest1 = checkinQueue.element();
-        print(checkinQueue);
-        System.out.println(guest1);
-
-//        poll removes while peek just peeks
-
-
+        print(messageStack);
     }
 
-    public static void print(Queue<Guest> queue) {
-
-        System.out.format("%n--Queue Contents--%n");
+    public static void print(Queue<String> dequeue) {
+        System.out.format("%n--Deques Contents--%n");
 
         int x = 0;
-        for (Guest guest : queue) {
-            System.out.format("%x: %s %s %n", x++, guest.toString(), x == 1 ? "(Head)" : "");
+        for (String msg : dequeue) {
+            System.out.format("%x: %s %s %n", x++, msg, x==1 ? "(Top)":"");
         }
-
         System.out.println("");
 
     }
